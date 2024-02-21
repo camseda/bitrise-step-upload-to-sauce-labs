c77d571bed42227ad44ed742076a73dd547f8e69
camilleseda <camille.seda@mavenclinic.com>
2024-02-21 17:13:32 -0500

    Remove empty space

--- step.sh
+++ step.sh
@@ -8,7 +8,6 @@ fi
 
 upload_path=$(if [[ $BUILD_TYPE == "apk" ]]; then echo $BITRISE_APK_PATH; elif [[ $BUILD_TYPE == "aab" ]]; then echo $BITRISE_AAB_PATH; else echo "$BITRISE_IPA_PATH"; fi)
 
-
 curl --location --request POST \
   --url "https://api.us-west-1.saucelabs.com/v1/storage/upload" \
   --user "$sauce_username:$sauce_access_key" \
